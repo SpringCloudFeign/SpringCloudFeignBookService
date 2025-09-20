50769e66d2ba099d6aba86669065eda63be2b28f
sivjee <sivjee@gmail.com>
2025-09-20 09:02:29 +0530

    initial commit

---
+++ src/main/java/com/example/SpringCloudFeignBookServiceApplication/SpringCloudFeignBookServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringCloudFeignBookServiceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringCloudFeignBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFeignBookServiceApplication.class, args);
+	}
+
+}
